6cdb489dbb988634a1fd128278f5568fbfafb028
Alex Pryakhin <npane0au>
2018-08-03 16:39:28 +0300

    now login returns daily token for succesfull auth

--- login.php
+++ login.php
@@ -8,15 +8,15 @@ define("SALT", "alk%#nq12HJ");
 
 $pair = [
     'login' => 'test',
-    'pass' => ''
+    'pass' => 'bc8e51c8dd2e98a9a0a792ce87416b59'
 ];
 
 if(isset($_POST['pass']) && isset($_POST['login'])){
     $salted = $_POST['pass'] . SALT;
     if(md5($salted) == $pair['pass']){
-        echo $token;
+        echo md5(date('Y-m-d').$salted);
     }
 }
 else{
-    echo md5('test'.SALT);
+    echo 0;
 }
